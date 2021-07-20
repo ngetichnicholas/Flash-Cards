6fd3582fabe22fa71efde9633b844b3d9582039e
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-20 19:50:07 +0300

    Add index view

--- flash_app/views.py
+++ flash_app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Card
+
 
 # Create your views here.
+@login_required
+def index(request):
+  cards = Card.show_cards()
+
+  return render (request,'index.html',{"cards":cards})
