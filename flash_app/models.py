7236759705343ba7dacf7e894e9e197279671e5f
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-21 20:40:46 +0300

    Add subject classmethods

--- flash_app/models.py
+++ flash_app/models.py
@@ -6,6 +6,24 @@ from django.contrib.auth.models import User
 class Subject(models.Model):
   name = models.CharField(max_length=50)
 
+  def save_subject(self):
+    self.save()
+
+  def delete_subject(self):
+      self.delete()
+
+  def update_subject(self, update):
+    self.name = update
+    self.save()
+
+  @classmethod
+  def get_subject_id(cls, id):
+    subject_id = Subject.objects.get(pk = id)
+    return subject_id
+
+  def __str__(self):
+    return self.name
+
 class FlashCard(models.Model):
   title = models.CharField(max_length=144)
   subject = models.ForeignKey(Subject,on_delete=models.CASCADE)
