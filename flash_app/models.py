7e57a8e6974fdc5e33250484ad5890bde020fc72
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-21 12:28:01 +0300

    Update Card model class

--- flash_app/models.py
+++ flash_app/models.py
@@ -1,10 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Card(models.Model):
-  pass
+  title = models.CharField(max_length=144)
+  subject = models.CharField(max_length=50)
+  front_side = models.TextField()
+  back_side = models.TextField()
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField()
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.title
 
   @classmethod
   def show_cards(cls):
     cards = cls.objects.all()
     return cards
+
+
