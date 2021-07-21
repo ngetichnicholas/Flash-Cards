175dee723b8736a959baf87abed2ce9cc354c739
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-21 20:30:20 +0300

    Add classes to admin site

--- flash_app/models.py
+++ flash_app/models.py
@@ -3,9 +3,12 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
+class Subject(models.Model):
+  name = models.CharField(max_length=50)
+
 class FlashCard(models.Model):
   title = models.CharField(max_length=144)
-  subject = models.CharField(max_length=50)
+  subject = models.ForeignKey(Subject,on_delete=models.CASCADE)
   front_side = models.TextField()
   back_side = models.TextField()
   created_at = models.DateTimeField(auto_now_add=True)
@@ -29,5 +32,7 @@ class FlashCard(models.Model):
 
   def __str__(self):
     return self.title
+
+
 
 
